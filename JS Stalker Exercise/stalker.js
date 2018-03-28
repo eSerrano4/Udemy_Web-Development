6a4e5f7572f1c07c96931cdba4cb2aed42fae470
beatjunkieeric <beatjunkieeric@gmail.com>
2018-03-27 19:07:49 -0700

    simple javaScript basics

---
+++ JS Stalker Exercise/stalker.js
@@ -0,0 +1,7 @@
+console.log("From the script file");
+var firstName = prompt("what is your first Name?");
+var lastName= prompt("what is your last name?");
+var age = prompt("how old are you?");
+
+console.log("Your Full name is " + firstName + " " + lastName);
+console.log("you are " + age + " " + "old.");
---
+++ Script Exercise 1/script.js
@@ -0,0 +1,3 @@
+var userName = prompt("what is your name?");
+alert("nice to meet you, " + userName);
+console.log("ALso great to meet you, "+ userName);
