e5f6d18053009d88e88f769284e2283ed6b87290
beatjunkieeric <beatjunkieeric@gmail.com>
2018-03-29 22:54:20 -0700

    while loops to show what a while loop actually does

---
+++ JS while loop assignments/whileLoop.js
@@ -0,0 +1,22 @@
+var num =1;
+
+while(num <= 10){
+    console.log(num);
+    num +=2;
+}
+
+var numTwo = 1;
+
+while(numTwo <= 20){
+    if(numTwo % 4 ===0){
+        console.log(numTwo);
+    }
+    numTwo++;
+}
+
+var numThree = 100;
+
+while(numThree < 150){
+    console.log(numThree + 1);
+    numThree--;
+}
